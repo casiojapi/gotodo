0f2ce63193639c32da1ca227a76d671c3541d4b5
maximo santoro <maximo.santoro@iovlabs.org>
2023-06-08 15:41:47 -0300

    add handlers

--- server.go
+++ server.go
@@ -20,5 +20,22 @@ func main() {
 	if port == "" {
 		port = "3000"
 	}
+
 	log.Fatalln(app.Listen(fmt.Spintf(":%v", port)))
+	
+	func indexHandler(c *fiber.Ctx) error {
+		return c.SendString("INDEX")
+	}
+
+	func postHandler(c *fiber.Ctx) error {
+		return c.SendString("POST")
+	}
+
+	func putHandler(c *fiber.Ctx) error {
+		return c.SendString("PUT")
+	}
+	
+	func deleteHandler(c *fiber.Ctx) error {
+		return c.SendString("DELETE")
+	}
 }
